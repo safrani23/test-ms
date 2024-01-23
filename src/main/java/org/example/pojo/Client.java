fa439c3b647c266553c54026bfe07145d1293ba8
NekrasovaEA1 <nekrasovaea1@tech.bcs.ru>
2024-01-23 15:14:54 +0300

    [MS-02.003] Added lombok annotations for pojo

--- src/main/java/org/example/pojo/Client.java
+++ src/main/java/org/example/pojo/Client.java
@@ -1,5 +1,10 @@
 package org.example.pojo;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
 public class Client {
     public String login;
 }
--- src/main/java/org/example/pojo/PositionList.java
+++ src/main/java/org/example/pojo/PositionList.java
@@ -1,7 +1,12 @@
 package org.example.pojo;
 
+import lombok.Builder;
+import lombok.Data;
+
 import java.util.ArrayList;
 
+@Builder
+@Data
 public class PositionList {
     public ArrayList<Position> positions;
 }
